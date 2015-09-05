ebc7c00faa0faa8c51409232fb0ffca305828747
cevherkarakoc <cevherkarakoc@live.om>
2015-09-05 17:32:29 +0300

    Folder Copy Added

---
+++ TCint/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCint
+{
+    class Program
+    {
+        
+
+        static int Main(string[] args)
+        {
+            string source,target;
+            if (args.Length == 0)
+            {
+                return 1;
+            }
+            else if (args.Length == 1)
+            {
+                source = args[0];
+                target = args[0] + "(COPY)";
+            }
+            else if (args.Length == 2)
+            {
+                source = args[0];
+                target = args[1];
+            }
+            else
+                return 2;
+
+            copyDir(source, target);
+
+            return 0;
+            
+        }
+
+        static void copyDir(string sourceDir, string targetDir)
+        {
+            Directory.CreateDirectory(targetDir);
+
+            foreach (var file in Directory.GetFiles(sourceDir))
+                File.Copy(file, Path.Combine(targetDir, Path.GetFileName(file)));
+        }
+    }
+
+}
